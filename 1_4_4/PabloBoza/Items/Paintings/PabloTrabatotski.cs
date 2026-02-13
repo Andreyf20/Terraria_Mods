e0f2bef601620f600345f799312b4107d00bbe4b
Andrey Torres <atorresf20@gmail.com>
2026-02-13 00:07:57 -0600

    Add Pablo Trabatotski painting

---
+++ 1_4_4/PabloBoza/Items/Paintings/PabloTrabatotski.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using ReLogic.Content;
+
+namespace PabloBoza.Items.Paintings
+{
+    public class PabloTrabatotski : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 48;
+            Item.height = 48;
+            Item.maxStack = 99;
+            Item.value = Item.buyPrice(0, 0, 0, 1); // 1 copper 
+            Item.rare = ItemRarityID.LightRed;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<Tiles.Paintings.PabloTrabatotskiTile>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.Wood, 1)
+                .AddTile(TileID.Sawmill)
+                .Register();
+        }
+    }
+}
---
+++ 1_4_4/PabloBoza/Tiles/Paintings/PabloTrabatotskiTile.cs
@@ -0,0 +1,34 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace PabloBoza.Tiles.Paintings
+{
+    public class PabloTrabatotskiTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileWaterDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall); // Needs wall or anchor doesn't work lmao
+            TileObjectData.newTile.Width = 5;
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16 };
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 0;
+            TileObjectData.newTile.Origin = new Point16(4, 3); // Anchor point
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(120, 85, 60), CreateMapEntryName());
+
+            // Can be placed on walls
+            TileID.Sets.DisableSmartCursor[Type] = true;
+        }
+    }
+}
